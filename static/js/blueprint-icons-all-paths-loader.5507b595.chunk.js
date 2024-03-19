aba7c901ab0c89bafc90651351c510a6810424e8
Ramon Garate <rg@rgs-macbook-air.lan>
2024-03-18 17:57:51 -0700

    features and bug fixes

--- static/js/blueprint-icons-all-paths-loader.5507b595.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpretzeldata=self.webpackChunkpretzeldata||[]).push([[824],{3975:(e,t,n)=>{n.r(t),n.d(t,{allPathsLoader:()=>r});var a=n(958),r=function(e,t){return(0,a.sH)(void 0,void 0,void 0,(function(){return(0,a.YH)(this,(function(a){switch(a.label){case 0:return[4,Promise.all([n.e(783),n.e(672),n.e(860)]).then(n.bind(n,4518))];case 1:return[2,(0,a.sent().getIconPaths)(e,t)]}}))}))}}}]);
-//# sourceMappingURL=blueprint-icons-all-paths-loader.5507b595.chunk.js.map
