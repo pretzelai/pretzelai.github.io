506ca5aa910350ff984e3bfd53a1585ebe11df54
Ramon Garate <rg@rgs-macbook-air.lan>
2024-04-05 10:51:49 -0700

    bug fixes

--- assets/splitPathsBySizeLoader-C2WqdxZk.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as o,a as c,b as s,p as l,I as u,__tla as h}from"./index-BLSuIh8s.js";let n,p=Promise.all([(()=>{try{return h}catch{}})()]).then(async()=>{n=function(_,i){return o(void 0,void 0,void 0,function(){var e,a;return c(this,function(t){switch(t.label){case 0:return e=l(_),i!==u.STANDARD?[3,2]:[4,s(()=>import("./index-BIdTI090.js").then(r=>r.I),__vite__mapDeps([]))];case 1:return a=t.sent(),[3,4];case 2:return[4,s(()=>import("./index-ClTF5qbq.js").then(r=>r.I),__vite__mapDeps([]))];case 3:a=t.sent(),t.label=4;case 4:return[2,a[e]]}})})}});export{p as __tla,n as splitPathsBySizeLoader};
